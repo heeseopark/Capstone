c27a34ae7f45fe688e81ce8af6ff92938c1da35c
heeseopark <heeseopark@korea.ac.kr>
2023-12-05 15:39:28 +0900

    1205 commit 2

--- test.py
+++ test.py
@@ -13,12 +13,20 @@ def subtract_stl(file1, file2, result_file):
     result_mesh.save(result_file)
 
 
-newhand = Mesh('newhand.stl').translate('z',8).rotate('z', 90).print_values().render()
+hand = Mesh('hand.stl').scale(20).smoothen().run_pca().rotate('x',-90).rotate('z',180).print_values().scale(1/20)
+
+scale_factor = 68 / float(hand.width)
+
+print("scale factor: " + str(scale_factor))
+
+hand.scale(scale_factor).translate('z', -hand.min_z_value).save('new3DModel.stl')
+
+newhand = Mesh('newhand.stl').translate('z',8).print_values()
 
 readyforsubtraction = newhand.save('readyforsubtraction.stl')
 
-base = Mesh('wristbase.stl').scale(1/4).render()
+base = Mesh('wristbase.stl').save('newwristbase.stl')
 
-subtract_stl('wristbase.stl', 'readyforsubtraction.stl', 'testresult.stl')
+subtract_stl('newwristbase.stl', 'readyforsubtraction.stl', 'testresult.stl')
 
 testresult = Mesh('testresult.stl').render()
